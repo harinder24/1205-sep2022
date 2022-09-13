b21b2a0c308b13fd2d06ee9b38a5225bb3238b23
000453980 <000453980@CC145XX1A019XXX>
2022-09-12 19:40:22 -0700

    Add project files.

---
+++ 1205-sep2022/1205-sep2022.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include "quiz1.h"
+#include "quiz1WithDouble.h"
+#include "assignment1_harinderpal_sran.h"
+
+int main()
+{
+    assignment1();
+    quiz1();
+    quiz1WithDouble();
+    
+}
+
+
