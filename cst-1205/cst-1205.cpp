c1878ff62dbb3a8ee999c41d91886c38f7532838
harinder24 <105934088+harinder24@users.noreply.github.com>
2022-09-13 14:49:57 -0700

    Add files via upload

---
+++ cst-1205/cst-1205.cpp
@@ -0,0 +1,29 @@
+// cst-1205.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "assignment1.h"
+#include "week2quiz1.h"
+#include "week2lab1.h"
+#include "week2lab2.h"
+#include "assignment2.h"
+
+int main()
+{
+	assignment1();
+	week2quiz1();
+	week2lab1();
+	week2lab2();
+	assignment2();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ cst-1205/week2lab1.h
@@ -0,0 +1,2 @@
+#pragma once
+void week2lab1();
