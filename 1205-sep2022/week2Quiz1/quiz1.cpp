7fc83cf21107d4aa0f87bcdd1eb1ecce268a09c8
Harinderpal Singh Sran <000453980@student.vcc.ca>
2022-09-12 20:40:24 -0700

    ds

---
+++ 1205-sep2022/week2Quiz1/quiz1.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+void quiz1()
+{
+
+	int firstFahrenheit = 0, secondFahrenheit = 0, thirdFahrenheit = 0;
+	std::cout << "Please enter three integer separated by space to change them from Fahrenheit to Celcius- ";
+	std::cin >> firstFahrenheit >> secondFahrenheit >> thirdFahrenheit;
+	int result1 = (firstFahrenheit - 32.0) * 5 / 9;
+	int result2 = (secondFahrenheit - 32.0) * 5 / 9;
+	int result3 = (thirdFahrenheit - 32.0) * 5 / 9;
+
+	std::cout << "\n" << firstFahrenheit << " changes to " << result1 << ", " << secondFahrenheit << " changes to " << result2 << " and " << thirdFahrenheit << " changes to " << result3 << "\n";
+
+
+}
