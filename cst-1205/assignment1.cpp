3efd4831c19c0cf227fcf7d2beca44bdf9fbe1dd
G. B. Karim <george.bekari@gmail.com>
2022-10-07 13:23:57 -0700

    Update assignment1.cpp

--- cst-1205/assignment1.cpp
+++ cst-1205/assignment1.cpp
@@ -16,7 +16,7 @@ void assignment1()
     std::cout << "Please enter character and integer seperated by space and press enter. ";
     std::cin >> char5 >> int5;
     double total = int1 + int2 + int3 + int4 + int5;
-    double average = total / 5;
+    double average = total / 5;   // [GK] : comment
     std::cout << "\nYour input is-\nYour first character and integer is " << char1 << " = " << int1 << "\nYour second character and integer is " << char2 << " = " << int2 << "\nYour third character and integer is " << char3 << " = " << int3 <<
         "\nYour fourth character and integer is " << char4 << " = " << int4 << "\nYour fifth character and integer is " << char5 << " = " << int5 <<
         "\n\nYour output-\nTotal value is- " << total << "\nAverage value is- " << average << "\n";
