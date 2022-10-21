0de7ef6c7681a10a84850bd6f27636829b82776f
harinder24 <hss022002@gmail.com>
2022-10-21 11:31:47 -0700

    d

--- cst-1205/quiz7.cpp
+++ cst-1205/quiz7.cpp
@@ -12,20 +12,20 @@ void quiz7() {
 	cin.getline(file1, file1Size);
 	cout << "\nNumber of lines you want to add in file: ";
 	cin >> numberOfLines;
-	fstream fout1(file1);
+	ofstream fout1(file1);
 	
 	while (i < numberOfLines) {
 		string userInput;
 		cout << "Type your line: ";
 		cin >> userInput;
-
+		fout1 << userInput << endl;
 		i++;
 	};
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	fout1.close();
 
 
-	fstream openFile(file1);
+	ifstream openFile(file1);
 	string line;
 	while (!openFile.eof()) {
 		getline(openFile, line);
